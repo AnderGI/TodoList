b67fea209bb63d188bac7c261b0feac5c5bdea7f
AnderGI <agibarguen@gmail.com>
2023-06-26 13:20:19 +0200

    Navbar added and styled

---
+++ src/JS/DOMCREATOR/ProjectCreator.js
@@ -0,0 +1,11 @@
+export const projectDOMRenderer = () => {
+  return `
+    <div class="container">
+    <span>
+      <input type="checkbox" />
+      <input type="text" placeholder="Type your projects name"/>
+    </span>
+    <span class="delete">&#128465;</span>
+  </div>
+    `;
+};
