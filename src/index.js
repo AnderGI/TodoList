fa9a4b6a68fe517435d20625dd262f28c69c01ea
AnderGI <agibarguen@gmail.com>
2023-06-26 13:33:10 +0200

    Aside section show and hide transition added

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import "./styles.css";
 import { $ } from "./JS/UTILITIES/Selectors";
+
+$("button.menu").addEventListener("click", function () {
+  $("body > main > aside").classList.toggle("hidden");
+});
