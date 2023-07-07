448cbeb37701380f716bbad073fc292711994946
AnderGI <agibarguen@gmail.com>
2023-07-07 15:06:44 +0200

    DOM todo and projects selectors added

--- src/JS/UTILITIES/Selectors.js
+++ src/JS/UTILITIES/Selectors.js
@@ -1,2 +1,7 @@
 export const $ = (selector) => document.querySelector(selector);
 export const $$ = (selector) => document.querySelectorAll(selector);
+//div.showMore || button.deleteBtn
+export const getDOMTodoFromElement = (element) =>
+  element.parentElement.parentElement;
+//button.expandContent || button.deleteContent
+export getDOMProjectFromElement = (element) => element.parentElement.parentElement.parentElement;
